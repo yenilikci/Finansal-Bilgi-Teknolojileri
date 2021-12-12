a7e13e8f62b55746d51b63aea35dcd2505c94e82
beyzaatosun <58009985+beyzaatosun@users.noreply.github.com>
2021-12-12 20:55:28 +0300

    resetpasswordscreen oluşturuldu.

---
+++ MobileProject/src/screens/resetpasswordscreen/ResetPasswordScreen.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Background from '../../components/background/Background'
+import BackButton from '../../components/backbutton/Backbutton'
+import Logo from '../../components/logo/Logo'
+import Header from '../../components/header/Header'
+import TextInput from '../../components/textinput/Textinput'
+import Button from '../../components/button/Button'
+import { emailValidator } from '../../helpers/emailValidator'
+import ResetPasswordScreenStyle from './ResetPasswordScreenStyle'
+
+export default function ResetPasswordScreen({ navigation }) {
+  const [email, setEmail] = useState({ value: '', error: '' })
+
+  const sendResetPasswordEmail = () => {
+    const emailError = emailValidator(email.value)
+    if (emailError) {
+      setEmail({ ...email, error: emailError })
+      return
+    }
+    navigation.navigate('LoginScreen')
+  }
+
+  return (
+    <Background>
+      <BackButton goBack={navigation.goBack} />
+      <Logo />
+      <Header>Restore Password</Header>
+      <TextInput
+        label="Email"
+        returnKeyType="done"
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+        description="You will receive email with password reset link."
+      />
+      <Button
+        mode="contained"
+        onPress={sendResetPasswordEmail}
+        style={{ marginTop: 16 }}
+      >
+        Send Instructions
+      </Button>
+    </Background>
+  )
+}
