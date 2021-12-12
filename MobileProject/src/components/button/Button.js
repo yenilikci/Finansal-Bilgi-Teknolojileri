1dc0394be10d9cc49d83437d3a22ff5a7fa3a18b
Ceyda <ceydaonemli2@gmail.com>
2021-12-12 20:29:07 +0300

    button component ve theme oluşturuldu

---
+++ MobileProject/src/components/button/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button as PaperButton } from 'react-native-paper'
+import { theme } from '../core/theme'
+import ButtonStyle from './ButtonStyle'
+
+export default function Button({ mode, style, ...props }) {
+  return (
+    <PaperButton
+      style={[
+        ButtonStyle.button,
+        mode === 'outlined' && { backgroundColor: theme.colors.surface },
+        style,
+      ]}
+      labelStyle={ButtonStyle.text}
+      mode={mode}
+      {...props}
+    />
+  )
+}
---
+++ MobileProject/src/components/button/ButtonStyle.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    button: {
+      width: '100%',
+      marginVertical: 10,
+      paddingVertical: 2,
+    },
+    text: {
+      fontWeight: 'bold',
+      fontSize: 15,
+      lineHeight: 26,
+    },
+  })
+  
