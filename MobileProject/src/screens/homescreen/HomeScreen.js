1ef7edb771925c76685a08b6a0aab46e413fac69
Ceyda <ceydaonemli2@gmail.com>
2021-12-12 21:19:48 +0300

    Home Screen ve Start Screen düzenlendi.

--- MobileProject/src/screens/homescreen/HomeScreen.js
+++ MobileProject/src/screens/homescreen/HomeScreen.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { Text,StyleSheet } from 'react-native'
+import { Text,StyleSheet, TouchableOpacity, View} from 'react-native'
 import { Card,Title,Paragraph, Badge } from 'react-native-paper'
 import CustomAppbar from '../../components/appbar/CustomAppbar'; 
+import Firebase from '../../config/firebase';
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}) {
+    const auth = Firebase.auth()
+
+    const logout = () => {
+        auth.signOut()
+        .then(() => {
+            navigation.replace("LoginScreen")
+        })
+        .catch(error  => alert(error.message))
+    }
+
     return (
         <>
            <CustomAppbar/>
+           <View style={styles.container}>
            <Text>
-               HomeScreen
+               {auth.currentUser?.email}
            </Text>
+           <TouchableOpacity
+           onPress={logout}
+           style={styles.button}>
+            <Text style={styles.buttonText}>Logout</Text>
+           </TouchableOpacity>
+           </View>
            <StatusBar style="light" />
         </>
     )
@@ -19,6 +37,20 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({  
     container:{  
         flex: 1,  
-        maxHeight: 150,
+        justifyContent: 'center',
+        alignItems: 'center'
     },   
+    button: {
+        backgroundColor: '#000',
+        width: '60%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 40
+    },
+    buttonText: {
+        color:'white',
+        fontWeight: '700',
+        fontSize: 16
+    }
 })  
--- MobileProject/src/screens/startscreen/StartScreen.js
+++ MobileProject/src/screens/startscreen/StartScreen.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Background from '../../components/background/Background'
 import Logo from '../../components/logo/Logo'
 import Header from '../../components/header/Header'
 import Button from '../../components/button/Button'
 import Paragraph from '../../components/paragraph/Paragraph'
 import StartScreenStyle from './StartScreenStyle'
+import Firebase from '../../config/firebase'
 
 export default function StartScreen({ navigation }) {
+
+  const auth = Firebase.auth()
+  
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if(user) {
+        navigation.replace("HomeScreen")
+      }
+    })
+    return unsubscribe
+  },[])
+
   return (
     <Background>
       <Logo />
